c2a801087acd35b3f09d88e5ec310485f529e581
RheaMimiCarillo <rheamimicarillo@gmail.com>
2022-08-16 23:32:38 -0700

    converted horned beast components to react Cards

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -1,4 +1,7 @@
 import React from 'react';
+
+// import Card component
+import Card from 'react-bootstrap/Card';
 import './HornedBeast.css';
 
 class HornedBeast extends React.Component{
@@ -23,31 +26,43 @@ class HornedBeast extends React.Component{
       favorites: this.state.favorites + 1,
     });
   };
-  
+
   render()
   {
     return(
-      <div className="hornedBeast">
-        {/* title of animal */}
-        <h2>{this.props.title}</h2>
-        <div className='imgContainer'>
-          {/* image of animal */}
-          <img 
-            src={this.props.imageURL} 
-            alt={this.props.description} 
-            title={this.props.title}
-            favorites={this.state.favorites}
-            onClick={this.handleFavorites}
-          />
-          {/* p to display votes for each beast */}
-          <p className='heart'>🖤{this.state.favorites}</p>
-        </div>
-        
-
-        
-        {/* description of animal */}
-        <p>{this.props.description}</p>
-      </div>
+      
+      // <div className="hornedBeast">
+      //   {/* title of animal */}
+      //   <h2>{this.props.title}</h2>
+      //   <div className='imgContainer'>
+      //     {/* image of animal */}
+      //     <img 
+      //       src={this.props.imageURL} 
+      //       alt={this.props.description} 
+      //       title={this.props.title}
+      //       favorites={this.state.favorites}
+      //       onClick={this.handleFavorites}
+      //     />
+      //     {/* p to display votes for each beast */}
+      //     <p className='heart'>🖤{this.state.favorites}</p>
+      //   </div>
+      //   {/* description of animal */}
+      //   <p>{this.props.description}</p>
+      // </div>
+      <Card className='hornedBeast' style={{width: '18rem'}}>
+        <Card.Img 
+          variant='top' 
+          src={this.props.imageURL} 
+          alt={this.props.description}
+          favorites={this.state.favorites}
+          onClick={this.handleFavorites}
+        />
+        <Card.Body>
+          <Card.Title>{this.props.title}</Card.Title>
+          <Card.Text>{this.props.description}</Card.Text>
+          <Card.Text>🖤{this.state.favorites}</Card.Text>
+        </Card.Body>
+      </Card>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
-// import Card from '@mui/material/Card';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
